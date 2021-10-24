f83f4a3817525f9fb2ac2cfabfe375f70ea3e5f3
DanailMitev <>
2021-10-24 20:49:38 +0000

    Ubung2

--- src/exercise2/DayOfWeek.java
+++ src/exercise2/DayOfWeek.java
@@ -2,30 +2,36 @@ package exercise2;
 import java.util.Scanner;
 public class DayOfWeek {
   public static void main(String[] args) {
-    
-Scanner input = new Scanner(System.in);
-System.out.print ("Enter a number: ");
-int number = input.nextInt();
+    int a;
 
-if (number == 1) {
-  System.out.println("Monday");
-} else if (number == 2) {
-  System.out.println("Tuesday");
+Scanner input = new Scanner(System.in);
+a = input.nextInt();
+switch(a) {
+  case 1:
+  System.out.println("Day of week: Monday");
+  break;
+  case 2:
+  System.out.println("Day of week: Tuesday");
+  break;
+  case 3:
+  System.out.println("Day of week: Wednesday");
+  break;
+  case 4:
+  System.out.println("Day of week: Thursday");
+  break;
+  case 5:
+  System.out.println("Day of week: Friday");
+  break;
+  case 6:
+  System.out.println("Day of week: Saturday");
+  break;
+  case 7:
+  System.out.println("Day of week: Sunday");
+  break;
+  default:
+  System.out.println("Day of week: Invalid day");
+  break;
 }
-else if (number == 3) {
-  System.out.println("Wednesday");
-} else if (number == 4) {
-  System.out.println("Thursday");
+input.close();
 }
-  else if (number == 5) {
-    System.out.println("Friday");
-  } else if (number == 6){
-    System.out.println("Saturday");
-  }
-  else if (number == 7) {
-    System.out.println("Sunday");
-  }else {
-    System.out.println("Invalid day");
-  }
-  }
 }
